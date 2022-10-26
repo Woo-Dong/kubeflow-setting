a54ededc025ec729f86d901b4fb5011ddb285397
Woo-Dong <dwpark2007@naver.com>
2022-10-26 13:27:44 +0900

    add user

--- 02_install_kubeflow.sh
+++ 02_install_kubeflow.sh
@@ -8,3 +8,8 @@ kubectl label node kube-worker-1 node-role.kubernetes.io/worker=worker
 # cd manifests
 
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+
+# sudo -E kubectl port-forward --address 0.0.0.0 svc/istio-ingressgateway -n istio-system 80:80
+
+# kustomize build example | kubectl apply -f -
+# kubectl rollout restart deployment dex -n auth
