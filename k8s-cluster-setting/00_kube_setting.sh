eacc1332ee3cdf12c9bde3e03cd27b884d5b0f46
Woo-Dong <dwpark2007@naver.com>
2023-05-31 15:07:19 +0900

    --prefix

--- k8s-cluster-setting/00_kube_setting.sh
+++ k8s-cluster-setting/00_kube_setting.sh
@@ -36,10 +36,14 @@ sudo sysctl --system
 
 
 # install kubelet, kubeadm
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl &&
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg &&
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list &&
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list &&
 sudo apt-get update
 sudo apt-get install -y kubelet=1.24.9-00 kubeadm=1.24.9-00 &&
 sudo apt-mark hold kubelet kubeadm kubectl
+
+# install nfs-client
+sudo apt-get install -y nfs-common
